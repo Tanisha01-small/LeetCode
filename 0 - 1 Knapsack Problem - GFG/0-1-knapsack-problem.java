eace20f05f49f1dc843ad2e13c5e47058c42aae1
TANISHA SINGHAL <88341606+Tanisha01-small@users.noreply.github.com>
2023-06-12 12:03:51 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -50,30 +50,52 @@ class gfg
 class Solution {
     private static int[][] dp;
 
-    static int knapSack(int W, int wt[], int val[], int n) {
+     static int knapSack(int W, int wt[], int val[], int n) {
         dp = new int[n + 1][W + 1];
-
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<W+1;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+            }
         }
-
-        return knapSackRecursive(W, wt, val, n);
-    }
-
-    private static int knapSackRecursive(int W, int wt[], int val[], int n) {
-        if (n == 0 || W == 0) {
-            return 0;
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<W+1;j++){
+                if(j>=wt[i-1]){
+                    dp[i][j]= Math.max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+                    }else{
+                        dp[i][j]=dp[i-1][j];
+        } 
+            }
         }
+      
+        
+        return dp[n][W];
+      // for (int[] row : dp) {
+        //     Arrays.fill(row, -1);
+        // }
+        
 
-        if (dp[n][W] != -1) {
-            return dp[n][W];
-        }
+    //     return knapSackRecursive(W, wt, val, n);
+    // }
 
-        if (W >= wt[n - 1]) {
-            return dp[n][W] = Math.max(val[n - 1] + knapSackRecursive(W - wt[n - 1], wt, val, n - 1),
-                    knapSackRecursive(W, wt, val, n - 1));
-        } else {
-            return dp[n][W] = knapSackRecursive(W, wt, val, n - 1);
-        }
+   // private static int knapSackRecursive(int W, int wt[], int val[], int n) {
+
+
+        // if (n == 0 || W == 0) {
+        //     return 0;
+        // }
+
+        // if (dp[n][W] != -1) {
+        //     return dp[n][W];
+        // }
+
+        // if (W >= wt[n - 1]) {
+        //     return dp[n][W] = Math.max(val[n - 1] + knapSackRecursive(W - wt[n - 1], wt, val, n - 1),
+        //             knapSackRecursive(W, wt, val, n - 1));
+        // } else {
+        //     return dp[n][W] = knapSackRecursive(W, wt, val, n - 1);
+        // }
     }
 }
