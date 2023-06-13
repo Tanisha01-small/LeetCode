0bf9abe1e0d03530bee7a7fd0b7c4a3345dabafb
TANISHA SINGHAL <88341606+Tanisha01-small@users.noreply.github.com>
2023-06-13 16:09:27 +0530

    Time: 49 ms (10.21%), Space: 53.6 MB (7.05%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -24,10 +24,6 @@ class Solution {
         
         
         -Memoization
-        -Top down approach
-        
-        common thing is first to check whether any string is empty or not our input should get smaller soo for next condition if the value is same then we can move infex of both strings if not we will have a choice for both the strings to first take one of the string complete and other one without the last element.
-        */
         
         int n=text1.length();
         int m=text2.length();
@@ -56,5 +52,34 @@ class Solution {
             return dp[s1][s2]= Math.max(longest(text1,text2,s1-1,s2),longest(text1,text2,s1,s2-1));
         }
         return 0;
+        
+        -Top down approach
+        
+        common thing is first to check whether any string is empty or not our input should get smaller soo for next condition if the value is same then we can move infex of both strings if not we will have a choice for both the strings to first take one of the string complete and other one without the last element.
+        */
+        int n=text1.length();
+        int m=text2.length();
+       
+        dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else if(text1.charAt(i-1)!=text2.charAt(j-1)){
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+        
+        
      }
 }
