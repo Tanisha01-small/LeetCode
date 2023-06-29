003d45729b2c90a70510ca65a85ae54e56d2a481
TANISHA SINGHAL <88341606+Tanisha01-small@users.noreply.github.com>
2023-06-29 16:18:25 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (96.92%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -16,23 +16,7 @@
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
         // we need to find the height excep the only condition is that you have to change root node to get the max
-//         if(root==null)return 0;
-//         int op1=diameterOfBinaryTree(root.left);
-//         int op2=diameterOfBinaryTree(root.right);
-//         int op3=height(root.left)+height(root.right);
-//         return Math.max(op1,Math.max(op2,op3));
-        
-//     }
-    
-//     private int height(TreeNode root){
-//         if(root==null)return 0;
-        
-//         int left=height(root.left);
-//         left++;
-//         int right=height(root.right);
-//         right++;
-        
-//         return Math.max(left,right);
+
         
         int[] dia=new int[1];
         height(root,dia);
