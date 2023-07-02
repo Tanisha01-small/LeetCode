9bc60e1d2e36138f429eabada0a695eb8bc40562
TANISHA SINGHAL <88341606+Tanisha01-small@users.noreply.github.com>
2023-07-02 16:56:37 +0530

    Time: 7 ms (99.65%), Space: 43.3 MB (92.65%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -10,30 +10,39 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
-        ArrayList<TreeNode> list1=new ArrayList<>();
-        ArrayList<TreeNode> list2=new ArrayList<>();
+//         ArrayList<TreeNode> list1=new ArrayList<>();
+//         ArrayList<TreeNode> list2=new ArrayList<>();
         
-        path(root,p,list1);
-        path(root,q,list2);
+//         path(root,p,list1);
+//         path(root,q,list2);
         
-        list1.retainAll(list2);
-        TreeNode ans=list1.get(list1.size()-1);
-        return ans;
+//         list1.retainAll(list2);
+//         TreeNode ans=list1.get(list1.size()-1);
+//         return ans;
+        
+        if(root==null || root==p|| root==q)return root;
+        
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        
+        if(left==null)return right;
+        else if(right==null)return left;
+        else return root;
     }
     
-    public boolean path(TreeNode root, TreeNode x,ArrayList<TreeNode> list){
-        if(root==null)return false ;
+//     public boolean path(TreeNode root, TreeNode x,ArrayList<TreeNode> list){
+//         if(root==null)return false ;
         
-        list.add(root);
-        if(root.val==x.val)return true ;
+//         list.add(root);
+//         if(root.val==x.val)return true ;
         
-        if(path(root.left,x,list) || path(root.right,x,list)){
-            return true; 
-        }
+//         if(path(root.left,x,list) || path(root.right,x,list)){
+//             return true; 
+//         }
         
-        list.remove(list.size()-1);
+//         list.remove(list.size()-1);
         
-        return false;
+//         return false;
         
-    }
+//     }
 }
